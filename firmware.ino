25de8c05fc4ab2c858d41ba9acf7ae40b608ed64
Shirel Cohen <shirel4201@gmail.com>
2026-06-11 15:04:14 +0300

    feat: send structured serial output and link potentiometer to blink rate

--- firmware.ino
+++ firmware.ino
@@ -2,40 +2,50 @@ const int LED_PIN = 25;
 const int BTN_PIN = 16;
 const int POT_PIN = 34;
 
-void setup_pins() 
+bool led_state = false;
+
+void setup_pins()
 {
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(BTN_PIN, INPUT_PULLUP);
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(BTN_PIN, INPUT_PULLUP);
 }
 
 int read_sensor()
 {
-  return analogRead(POT_PIN);
+    return analogRead(POT_PIN);
 }
 
-void update_led()
+void setup()
 {
-  int btn_state = digitalRead(BTN_PIN);
-
-  if (btn_state == LOW)
-  {
-    digitalWrite(LED_PIN, HIGH);
-  }
-  else
-  {
-    digitalWrite(LED_PIN, LOW);
-  }
+    Serial.begin(115200);
+    setup_pins();
 }
 
-void setup() 
+void loop()
 {
-  Serial.begin(115200);
-  setup_pins();
-}
+    int sensor_val  = read_sensor();
+    int btn_raw     = digitalRead(BTN_PIN);
+    int btn_state   = (btn_raw == LOW) ? 1 : 0;
 
-void loop() 
-{
-  update_led();
-  Serial.println(read_sensor());
-  delay(500);
+    int blink_delay = map(sensor_val, 0, 4095, 1000, 50);
+
+    if (btn_state == 1)
+    {
+        digitalWrite(LED_PIN, HIGH);
+        led_state = true;
+        delay(blink_delay);
+    }
+    else
+    {
+        digitalWrite(LED_PIN, HIGH);
+        led_state = true;
+        delay(blink_delay / 2);
+
+        digitalWrite(LED_PIN, LOW);
+        led_state = false;
+        delay(blink_delay / 2);
+    }
+
+    Serial.printf("sensor=%d,button=%d,led=%s\n",
+                  sensor_val, btn_state, led_state ? "ON" : "OFF");
 }
